c2c01035b7b13efc155558777b0c030b9479fbdf
Lautaro Mirenda <lautaro_fes@hotmail.com.ar>
2016-03-25 01:36:08 -0300

    Clase Usuario

--- src/main/java/bd2/model/Usuario.java
+++ src/main/java/bd2/model/Usuario.java
@@ -1,5 +1,62 @@
 package bd2.model;
 
-public class Usuario {
+import java.util.*;
 
+
+public class Usuario {
+	private String email;
+	private String nombre;
+	private Date fechaDeCreacion;
+	private Collection<Cursada> cursadasRealizadas = new LinkedList<Cursada>();
+	private Collection<Traduccion> traducciones = new LinkedList<Traduccion>();
+	
+	public Usuario(String email, String nombre, Date fechaDeCreacion){
+		this.email = email;
+		this.nombre = nombre;
+		this.fechaDeCreacion = fechaDeCreacion;
+	}
+	
+	public int nivel(Idioma idioma){
+		return 0; //Hacer.
+	}
+	
+	public Collection<Cursada> cursadasAprobadas(Idioma idioma){
+		return new LinkedList<Cursada>(); //Hacer.
+	}
+	
+	public void agregarCursada(Cursada cursada){
+		this.cursadasRealizadas.add(cursada);
+	}
+	
+	public void agregarTraduccion(Traduccion traduccion){
+		this.traducciones.add(traduccion);
+	}
+	
+	public Collection<Cursada> getCursadasRealizadas(){
+		return this.cursadasRealizadas;
+	}
+	
+	public Collection<Traduccion> getTraducciones(){
+		return this.traducciones;
+	}
+	
+	public void setEmail(String email){
+		this.email=email;
+	}
+	
+	public void setNombre(String nombre){
+		this.nombre=nombre;
+	}
+	
+	public void setDate(Date fechaDeCreacion){
+		this.fechaDeCreacion=fechaDeCreacion;
+	}
+	
+	public void setCursadasRealizadas(Collection<Cursada> cursadasRealizadas){
+		this.cursadasRealizadas=cursadasRealizadas;
+	}
+	
+	public void setTraducciones(Collection<Traduccion> traducciones){
+		this.traducciones=traducciones;
+	}
 }
