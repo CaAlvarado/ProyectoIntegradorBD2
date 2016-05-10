ec34958297de0b8db185890317aeae34447a72d3
CaAlvarado <ca3212@gmail.com>
2016-05-10 19:53:55 -0300

    Consultas agregadas

--- src/main/java/bd2/util/Queries.java
+++ src/main/java/bd2/util/Queries.java
@@ -22,6 +22,8 @@ public class Queries {
 		Session session = sesion.openSession();
 		try {
 			consultaA(session);
+			consultaC(session);
+			consultaG(session, "Leuchtturm");
 		}
 		catch (Exception e){
 			e.printStackTrace();
@@ -48,6 +50,89 @@ public class Queries {
 			}
 			System.out.println();
 		} catch (HibernateException e){
+			e.printStackTrace();
+		}
+		catch (Exception e){
+			e.printStackTrace();
+			if (tx != null){
+				tx.rollback();
+			}
+		}
+	}
+	
+	private static void consultaC(Session session){
+		Transaction tx = null;
+		System.out.println("c. Listar los usuarios que hayan iniciado una cursada de Francés de nivel 3 como mínimo");
+		
+		Query query = session.createQuery("select distinct usuario from "+Usuario.class.getName()+" usuario join usuario.cursadasRealizadas cr where cr.curso.idioma.nombre like 'Francés' and cr.curso.nivel > 2");
+		
+		
+		try {
+			tx = session.beginTransaction();
+			List<Usuario> usuarios= query.list();
+			tx.commit();
+			session.flush();
+			for (Usuario u: usuarios){
+				System.out.println("Nombre: "+u.getNombre());
+			}
+			System.out.println();
+		}
+		catch (HibernateException e){
+			e.printStackTrace();
+		}
+		catch (Exception e){
+			e.printStackTrace();
+			if (tx != null){
+				tx.rollback();
+			}
+		}
+	}
+	
+	private static void consultaE(Session session){
+		Transaction tx = null;
+		System.out.println("e. Listar traducciones completas del Inglés al Francés");
+		
+		Query query = session.createQuery("select traduccion from "+Traduccion.class.getName()+" traduccion join traduccion.parrafo parrafo where parrafo.documento.idioma.nombre like 'Inglés' and traduccion.completa = 1 and traduccion.idioma.nombre like 'Francés'");
+		
+		
+		try {
+			tx = session.beginTransaction();
+			List<Traduccion> traducciones= query.list();
+			tx.commit();
+			session.flush();
+			for (Traduccion t: traducciones){
+				System.out.println("Nombre: "+t.getDescripcion());
+			}
+			System.out.println();
+		}
+		catch (HibernateException e){
+			e.printStackTrace();
+		}
+		catch (Exception e){
+			e.printStackTrace();
+			if (tx != null){
+				tx.rollback();
+			}
+		}
+	}
+	
+	private static void consultaG(Session session, String palabra){
+		Transaction tx = null;
+		System.out.println("g. Obtener el nombre del idioma que define la palabra enviada como parámetro en su diccionario");
+		
+		Query query = session.createQuery("select diccionario from "+Diccionario.class.getName()+" diccionario where exists ( from "+Diccionario.class.getName()+" diccionario join diccionario.definiciones definiciones where definiciones)");
+		query.setParameter("palabra", palabra);
+		
+		
+		try {
+			tx = session.beginTransaction();
+			Diccionario diccionario = (Diccionario) query.uniqueResult();
+			tx.commit();
+			session.flush();
+			System.out.println("El idioma "+diccionario.getIdioma().getNombre()+" define la palabra "+palabra);
+			System.out.println();
+		}
+		catch (HibernateException e){
 			e.printStackTrace();
 		}
 		catch (Exception e){
